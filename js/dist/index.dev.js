443683379f7af507d2555aa9366db09a6e7f9122
Oumar <oumarmauret@gmail.com>
2021-08-22 16:02:37 +0000

    Refresh feature and add new feature

---
+++ js/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox({
+    alwaysShowClose: true,
+    showArrows: true
+  });
+});
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox({
+    alwaysShowClose: true,
+    showArrows: true,
+  });
+});
